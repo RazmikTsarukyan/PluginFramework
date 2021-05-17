716da425e350833679870c41805bc3fe3fdf8201
Razmik <Razmik@DESKTOP-EGK7T9A>
2021-05-17 22:51:43 +0400

    commit project

---
+++ PluginFramework/Controllers/PluginController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PluginFramework.Models;
+
+namespace PluginFramework.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class PluginController : Controller
+	{
+		private static List<PluginImg> pluginImgs = new List<PluginImg>();
+		
+		[HttpGet("getall")]
+		public IActionResult Index()
+		{
+			return Ok(pluginImgs);
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult Get(int id)
+		{
+			PluginImg pluginImg = pluginImgs.FirstOrDefault(p => p.Id == id);
+
+			if (pluginImg == null)
+				return NotFound("Not Found");
+
+			return Ok(pluginImg);
+		}
+
+		[HttpPost("Create")]
+		public IActionResult Post(PluginImg pluginImg)
+		{
+			pluginImgs.Add(pluginImg);
+
+			return Ok(pluginImgs);
+		}
+
+		[HttpPut("Update")]
+		public IActionResult Put(PluginImg pluginImg)
+		{
+			PluginImg img = pluginImgs.FirstOrDefault(p => p.Id == pluginImg.Id);
+
+			if (img == null)
+				return NotFound("Not Found");
+
+			pluginImgs.Remove(img);
+			pluginImgs.Add(pluginImg);
+
+			return Ok(pluginImgs);
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			PluginImg pluginImg = pluginImgs.FirstOrDefault(p => p.Id == id);
+			
+			if(pluginImg == null)
+				return NotFound("Not Found");
+
+			pluginImgs.Remove(pluginImg);
+
+			return Ok(pluginImgs);
+		}
+	}
+}
---
+++ PluginFramework/Models/PluginImg.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PluginFramework.Models
+{
+	public class PluginImg
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public double Radius { get; set; }
+		public double Size { get; set; }
+		public string Effect1 { get; set; }
+		public string Effect2 { get; set; }
+		public string Effect3 { get; set; }
+
+	}
+}
